597feab8665c56f49a013a81c56579f1b5547fa9
izzy417 <israeldail2@gmail.com>
2022-09-15 01:06:41 +0000

    progress

--- src/models.py
+++ src/models.py
@@ -14,6 +14,10 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    nickname = Column(String(250), nullable=False)
+    website = Column(String(250), nullable=False)
+    biography = Column(String(500), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
@@ -29,10 +33,39 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    location = Column(String(250), nullable=False)
+    image = Column(String(250), nullable=False)
+    likes = Column(String(250), nullable=False)
+    comments = Column(String(250), nullable=False)
+    message = Column(String(500), nullable=False)
+    favorite = Column(String(250), nullable=False)
+    person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+
+class Search(Base):
+    __tablename__ = 'search'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    profile = Column(String(250), nullable=False)
+    favorite = Column(String(250), nullable=False)
+    search_bar = Column(String(250), nullable=False)
+    person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
     print("There was a problem genering the diagram")
-    raise e
+    raise 
